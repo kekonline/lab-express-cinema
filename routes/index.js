21da19b82ccf4081f2cf118e63a07a84b87bfa94
kekonline <kekonline@gmail.com>
2023-08-05 17:01:32 +0200

    all done with love and care tried to do asycn / await

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const Movies = require("../models/Movie.model.js")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    Movies.find()
+        .then((allMoviesResponse) => {
+            res.render("movies.hbs", { allMovies: allMoviesResponse })
+        })
+        .catch((error) => console.log(error))
+})
+
+router.get("/movies/:id", (req, res, next) => {
+    Movies.findById(req.params.id)
+        .then((oneMovieResponse) => {
+            res.render("movie.hbs", { oneMovie: oneMovieResponse })
+        })
+        .catch((error) => console.log(error))
+})
+
+
+
+
 
 module.exports = router;
